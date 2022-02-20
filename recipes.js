78136169f1b6debab7d736438c3bedf57a9c07d6
Cheng Adrian <chengadrian@ChengdeMacBook-Pro.local>
2022-02-20 21:03:19 +0000

    filter

--- recipes.js
+++ recipes.js
@@ -9,20 +9,37 @@ const recipes = {
     stoneGlass:{
         stone:1,
         glass:3
+    },
+    filter:{
+        glass:5,
+        wood:5,
+        stone:5
     }
 }
 
 const furnaceRecipes = [
-    {raw:'sand',result:'glass'}
+    {raw:'sand',result:'glass'},
+    {raw:'iron',result:'ironIngot'}
 ]
 
 const furnaceFuels = ['wood','compressedWood']
 
 const rawTimes = {
-    sand:100
+    sand:100,
+    iron:300
 }
 
 const fuelTimes = {
     wood:100,
     compressedWood:300
+}
+
+const filterRecipes = {
+    sand:[
+        {prob:100,result:'iron'}
+    ]
+}
+
+const filterTimes = {
+    sand:1000
 }
