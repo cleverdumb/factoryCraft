287ba3943bde55a001c9a1155a9fe7993416badc
Cheng Adrian <chengadrian@ChengdeMacBook-Pro.local>
2022-02-26 11:54:19 +0000

    Save and better quests

--- quests.js
+++ quests.js
@@ -1,31 +1,34 @@
 console.log('%cquests loaded','color:cyan')
 
 class Quest {
-    constructor(name,parent,checkReq,double,detail) {
+    constructor(name,checkReq,detail) {
         this.name = name;
-        this.parentIndex = parent;
+        this.index = 0;
         this.checkReq = checkReq;
         this.completeCount = 0;
         this.completed = false;
-        this.double = double;
         this.expanded = false;
         this.detail = detail;
+        this.enabled = false;
     }
     init() {
-        this.parent = this.parentIndex!=0?quests[this.parentIndex-1]:new Root();
-        this.checkReq();
+        quests.forEach((x,i)=>{
+            if (x.name == this.name) {
+                this.index = i;
+            }
+        })
     }
     complete() {
         this.completeCount++;
-        if (this.completeCount==1+(this.double)?1:0) {
+        if (this.completeCount==1) {
             this.realComplete();
         }
     }
     realComplete() {
         this.completed = true;
         this.enabled = false;
-        if (this.parentIndex<quests.length-1) {
-            quests[this.parentIndex+1].enabled = true;
+        if (this.index<quests.length-1) {
+            quests[this.index+1].enabled = true;
         }
         game.drawQuests();
     }
@@ -36,26 +39,30 @@ class Root {
 }
 
 const quests = [
-    new Quest('The beginning',0,()=>{
-        complete(0)
-    },true,'This is the beginning of this.\nPress \'Check/submit\' to complete this quest!'),
-    new Quest('first resource',1,()=>{
+    new Quest('The beginning',()=>{
+        complete('The beginning')
+    },'This is the beginning of this.\nPress \'Check/submit\' to complete this quest!'),
+    new Quest('first resource',()=>{
         if (Object.keys(player.inv).length>0) {
-            complete(1);
+            complete('first resource');
         }
-    },true,'You can walk around with WASD.\nMove aside to a piece of resource (yellow, brown, and grey).\nThen press the resource on the minimap (bottom-left).\nPress it enough time and it will break and you will get the resource!'),
-    new Quest('first craft',2,()=>{
+    },'You can walk around with WASD.\nMove aside to a piece of resource (yellow, brown, and grey).\nThen press the resource on the minimap (bottom-left).\nPress it enough time and it will break and you will get the resource!'),
+    new Quest('first craft',()=>{
         for (item in player.inv) {
             if (recipes.hasOwnProperty(item) || game.furnace.enabled || game.filterEmpty) {
-                complete(2);
+                complete('first craft');
                 break;
             }
         }
-    },true,'After getting enough materials, go to the leftmost side of the map.\nThen go even left, you will go to another room.\nCraft something by clicking one of the recipe boxes.')
+    },'After getting enough materials, go to the leftmost side of the map.\nThen go even left, you will go to another room.\nCraft something by clicking one of the recipe boxes.')
 ]
 
-function complete(i) {
-    quests[i].complete();
+function complete(name) {
+    quests.forEach(x=>{
+        if (x.name == name) {
+            x.complete();
+        }
+    })
 }
 
 quests.forEach(x=>x.init())
@@ -69,3 +76,5 @@ function questToggle(name) {
     })
     game.drawQuests();
 }
+
+// game.restoreQuests();
